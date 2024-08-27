c6831f10bf689ec1a2828759f87d5be7a1ea273f
devsubhanshu <subhanshukumargupta41@gmail.com>
2024-08-27 20:36:35 +0530

    initial commit

---
+++ PraRoz/console.js
@@ -0,0 +1,14 @@
+const profile = {
+username: "subhanshu kumar",
+isfollow: "false",
+followers: "123",
+following: "123",
+
+};
+
+// comperesion operators
+let a = 5
+let b = 3
+
+
+console.log("a==b", a==b)
---
+++ javaskip/javascript.js
@@ -0,0 +1,158 @@
+// const a = {
+// 	folders: [
+// 		{
+// 			path: "."
+// 		}
+// 	],
+// 	settings: {}
+// };
+
+// var b = 13 * 5;
+// console.log(b)
+//  document.body.insertAdjacentHTML("afterend" , `<h1>${b}</h1>`)
+
+// fullname = "subhanshu kumar gupta"
+// console.log(fullname)
+
+// let x = BigInt("123")
+// let y = Symbol("hello")
+
+
+// const student = {
+// 	fullname: "Subhanshu Kumar",
+// 	age: 19,
+// 	Cgpa: 4.2,
+// 	Ispass: true,
+// }
+
+// student["age"] = student["age"] + 1;
+
+
+
+// console.log(student["age"]);
+
+// const profile = {
+// 	username: "subhanshu kumar",
+// 	isfollow: "false",
+// 	followers: "123",
+// 	following: "123",
+
+// };
+
+// console.log(typeof profile["username"]);
+// arithemetic operator
+// let A = 5;
+// let B = 7;
+
+// console.log("A= ", A, "&B= 7",);
+// console.log("A+B=", A+B);
+// console.log("A-B=", A-B);
+// console.log("A*B=", A*B);
+// console.log("A/B=", A/B);
+// console.log("A**B=", A**B);
+
+
+/*  Unary operator
+let A = 5;
+let B = 7;
+
+console.log("A= ", A, "&B= 7",);
+ A++; // 6
+console.log("A=", A)
+ B--; //6
+  console.log("B=",B);*/
+
+
+
+// console.log(add(12,3)) //5
+
+// const add = (a, b) => a + b;
+// const sqr = (a) => a ** 2;
+	// console.log("A= 2", A, "&B= 3",);
+// console.log("A+B=", A + B);
+
+// console.log(add(30, 23));
+// console.log(add(300, 2));
+// console.log(add(20, 8));
+// console.log(add(130, 2));
+
+
+
+//assignment operators
+// let a = 5
+// let b = 3
+
+
+// console.log("a=b", a=b);
+// console.log("a+=b", a+=b)
+// console.log("a-=b", a-=b)
+// console.log("a*=b", a*=b)
+// console.log("a%=b", a%=b)
+
+// comperesion operators
+// let a = 5
+// let b = "3"
+
+
+// console.log("a==b", a==b);
+// console.log("a===b", a===b);
+// console.log("a!=b", a!=b);
+// console.log("a!==b", a!==b);
+// console.log("a>b", a>b);
+// console.log("a>=b", a>=b);
+// console.log("a<=b", a<=b)
+
+// logical operators
+// let a = 5
+// let b = 3
+
+// let cond1 = a>b;
+// let cond2 = a===b;
+
+// console.log("cond1 && cond2=", cond1 && cond2);
+// console.log("cond1 || cond2=", cond1 || cond2);
+// console.log("!cond2=" , !cond2);
+
+
+// condition statement
+// let age = 16;
+// if (age >= 18){
+//   console.log("you can vote")
+// }
+// else
+// console.log("you can not vote")
+ 
+// odd or even
+// let num = 15;
+
+// if (num%2===0){
+// console.log(num, "is even");
+// }
+// else{
+// console.log(num, "is odd");
+// }
+
+
+// let age =45;
+
+// if (age>=18){
+//   console.log("junior");}
+// else if(age<=70){
+//   console.log("senior");
+// }
+// else(age18<=69)
+// console.log("middle");
+
+
+
+// if(mode==="none"); {
+//   console.log(mode);
+// }
+
+let age=25;
+let result= age >= 18 ? "adult" : "not adult";
+console.log(result);
+
+
+
+
